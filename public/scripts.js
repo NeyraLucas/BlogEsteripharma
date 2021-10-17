56fcee1ffbe470bdbfea6dd02700c976f7692daf
NeyraLucas <luiscrazypeople@gmail.com>
2021-10-17 16:18:27 -0500

    Se completo el modeilo de pharma

---
+++ public/scripts.js
@@ -0,0 +1,20 @@
+const info = [
+    {
+        id: 1,
+    },
+    {
+        id:2,
+    },
+    {
+        id: 4,
+    },
+    {
+        id:5,
+    }
+];
+
+
+function imprimirModal(numero){
+    console.log(info[numero].id);
+    alert(info[numero].id);
+}
